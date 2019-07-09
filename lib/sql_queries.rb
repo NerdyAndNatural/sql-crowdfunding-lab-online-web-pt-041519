8ad75e2e163af9c2dde1aa5b0c7a0a2528b1a72b
Sarah Williams <srwilliams8678@gmail.com>
2019-07-09 17:54:39 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -58,3 +58,7 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
   JOIN pledges ON projects.id = pledges.project_id
   WHERE projects.category = 'books';"
 end
+
+end
+end
+end
