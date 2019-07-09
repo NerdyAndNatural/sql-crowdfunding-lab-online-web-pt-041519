049137641648b35281ace3ffbb96d50448d5a6d8
Sarah Williams <srwilliams8678@gmail.com>
2019-07-09 17:52:34 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -6,9 +6,13 @@
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
-def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
-  "SELECT name, age FROM bears WHERE gender = 'F';"
+def selects_the_titles_of_all_projects_and_their_pledge_amounts
+"Write your SQL query Here"	  "SELECT projects.title, SUM(pledges.amount)
+  FROM projects
+  INNER JOIN pledges
+  ON projects.id = pledges.project_id
+  GROUP BY projects.id
+  ORDER BY projects.title;"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
